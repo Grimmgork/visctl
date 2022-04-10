4f89db74b65fce7fe2e444d361021f1c9c22f5e4
Grimmgork <mmmru7shj@gmail.com>
2022-04-10 13:06:59 +0100

    Addet the Template-controller The Template-Controller middleware will provide a way to edit the templates-directory specified in the config file. you can upload a file by posting the contents at the desired endpoint.

---
+++ app.js
@@ -0,0 +1,120 @@
+const express = require('express');
+const fs = require('fs');
+const pth = require('path');
+const favicon = require('serve-favicon');
+const mime = require('mime-types');
+
+const vbk = require('./view');
+const config = require('./config.json');
+
+
+let app = express();
+
+let state = LoadState();
+vbk.SwitchContent(state.template);
+
+app.use(favicon('./static/favicon.ico'));
+app.use(TemplateManager);
+app.use('/static', express.static('./static'));
+
+
+function TemplateManager (req, res, next){
+	const templateDirectory = config.templates;
+	let path = pth.normalize(req.path).replaceAll(/^\/|\/$/g,'').split("/");
+
+	if(path[0] == 'templates'){
+		path.shift();
+		path = pth.join(templateDirectory, path.join("/"));
+
+		if(req.method == 'GET'){
+			if(fs.existsSync(path)){
+				if(fs.lstatSync(path).isDirectory()){
+					res.send(fs.readdirSync(path).sort(function(a,b){
+						const ad = fs.lstatSync(pth.join(path, a)).isDirectory();
+						const bd = fs.lstatSync(pth.join(path, b)).isDirectory();
+						if(bd == ad)
+							return 0;
+						if(bd)
+							return 1;
+						
+						return -1;
+					}));
+					return;
+				}
+				else{
+					res.setHeader("Content-Type", mime.lookup(path));
+					res.send(fs.readFileSync(path));
+					return;
+				}
+			}
+			else
+			{
+				res.sendStatus(404);
+				return;
+			}
+		}
+		else
+		if(req.method == 'POST'){
+			const requestBody = [];
+      		req.on('data', (chunks)=>{
+				requestBody.push(chunks);
+      		});
+
+			req.on('end', ()=>{
+				const data = Buffer.concat(requestBody).toString();
+				fs.writeFileSync(path, data);
+		  	});
+			res.send();
+			return;
+		}
+	}
+	next();
+}
+
+function GetSubdirectories(dir){
+	return fs.readdirSync(dir).filter((f) => fs.lstatSync(pth.join(dir, f)).isDirectory());
+}
+
+function ReadFileFromRoot(root, path){
+	let fullPath = pth.join(root, pth.normalize(path)); 
+	return fs.readFileSync(fullPath);
+}
+
+function LoadState(){
+	const path = './state.json';
+	if(!fs.existsSync(path)){
+		return {template: null}
+	}
+	return require(path);
+}
+
+function SaveState(state){
+	fs.writeFile('./state.json', JSON.stringify(state), function(){});
+}
+
+
+app.get('/view', function(req, res){
+	let content = fs.readFileSync('./static/view/index.html');
+	content = content.toString().replaceAll('{WEBSOCKETPORT}', config.wssPort);
+	res.send(content);
+});
+
+app.get('/switch', function(req, res){
+	res.send(vbk.GetContent());
+});
+
+app.post('/switch/:templatename', function(req, res){
+	const templatename = req.params.templatename;
+	if(GetSubdirectories(config.templates).filter((f) => f == templatename).length == 0){
+		res.sendStatus(404);
+		return;
+	}
+
+	vbk.SwitchContent(templatename);
+	state.template = vbk.GetContent();
+	SaveState(state);
+
+	res.send();
+});
+
+app.listen(5000);
